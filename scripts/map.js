ddc21131e3e031d4ee9d864f7146fa422b589e39
FlowCAD <florian.cadoz@gmail.com>
2017-02-14 15:58:49 +0100

    Create map.js
    Creating the map with Openlayers v4 !

---
+++ scripts/map.js
@@ -0,0 +1,15 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([1.45556, 43.6066686]),
+        zoom: 12
+    }),
+		controls: ol.control.defaults().extend([
+        new ol.control.FullScreen()
+    ])
+});
