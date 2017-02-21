49fd7b19d8fc060362174588f6040b588fd6eef1
FlowCAD <florian.cadoz@gmail.com>
2017-02-21 15:15:42 +0100

    Update map.js
    Adding geolocation of user and jslint options !

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,15 @@
+/*jslint node: true*/
+/*global ol*/
+"use strict";
+
+var view = new ol.View({
+    projection: 'EPSG:4326',
+    center: [1.45556, 43.6066686],
+    zoom: 12,
+    minZoom: 11,
+    maxZoom: 20
+});
+
 var map = new ol.Map({
     target: 'map',
     layers: [
@@ -5,15 +17,60 @@ var map = new ol.Map({
             source: new ol.source.OSM()
         })
     ],
-    view: new ol.View({
-        projection: 'EPSG:4326',
-        center: [1.45556, 43.6066686],
-        zoom: 12,
-        minZoom: 11,
-        maxZoom: 20
-    }),
     controls: ol.control.defaults().extend([
         new ol.control.FullScreen(),
         new ol.control.ScaleLine()
-    ])
+    ]),
+    view: view
+});
+
+var geolocation = new ol.Geolocation({
+    projection: view.getProjection()
+});
+
+
+function el(id) {
+    return document.getElementById(id);
+}
+
+el('track').addEventListener('change', function () {
+    geolocation.setTracking(this.checked);
+});
+
+geolocation.on('error', function (error) {
+    var info = document.getElementById('info');
+    info.innerHTML = error.message;
+    info.style.display = '';
+});
+
+var accuracyFeature = new ol.Feature();
+geolocation.on('change:accuracyGeometry', function () {
+    accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+var positionFeature = new ol.Feature();
+positionFeature.setStyle(new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 6,
+        fill: new ol.style.Fill({
+            color: '#3399CC'
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#fff',
+            width: 2
+        })
+    })
+}));
+
+geolocation.on('change:position', function () {
+    var coordinates = geolocation.getPosition();
+    positionFeature.setGeometry(coordinates ? new ol.geom.Point(coordinates) : null);
+});
+
+
+new ol.layer.Vector({
+    map: map,
+    source: new ol.source.Vector({
+        features: [accuracyFeature, positionFeature]
+    })
 });
